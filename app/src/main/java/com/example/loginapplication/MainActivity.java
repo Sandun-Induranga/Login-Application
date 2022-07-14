55ce619556cee95228fd287daac5b6883dc0b60c
Sandun-Induranga <sandutharu40@gmail.com>
2022-07-14 09:44:11 +0530

    Create login logic

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.loginapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Wrong Login Details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
